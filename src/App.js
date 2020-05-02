8b67606e47bc303ca9ae992d9b62ef1b816c4fe7
travischantravis <chancheukyin4463@gmail.com>
2020-05-02 14:27:07 -0700

    axis

--- src/App.js
+++ src/App.js
@@ -1,36 +1,66 @@
 // Let d3 do the DOM instead of react
 import React, { useRef, useEffect, useState } from "react";
-import { select, line, curveCardinal } from "d3";
+import {
+  select,
+  line,
+  curveCardinal,
+  axisBottom,
+  axisRight,
+  scaleLinear,
+} from "d3";
 import "./App.css";
 
 function App() {
+  // width is 300px?
   const svgRef = useRef();
 
-  const [data, setData] = useState([25, 30, 45, 60, 20]);
+  const [data, setData] = useState([25, 30, 45, 60, 20, 60, 75]);
 
   // Line graph, path allows curves
   useEffect(() => {
-    console.log(svgRef);
     const svg = select(svgRef.current);
+
+    const xScale = scaleLinear()
+      .domain([0, data.length - 1])
+      .range([0, 300]);
+
+    // domain 0 will be mapped to 150, while domain 75 will be mapped to 0
+    const yScale = scaleLinear().domain([0, 75]).range([150, 0]);
+
+    const xAxis = axisBottom(xScale)
+      .ticks(data.length)
+      .tickFormat((index) => index + 1);
+    const yAxis = axisRight(yScale);
+
+    svg.select(".x-axis").style("transform", "translateY(150px)").call(xAxis);
+    svg.select(".y-axis").style("transform", "translateX(300px)").call(yAxis);
+
+    // Generate the d attribute of a path element
     const myLine = line()
-      .x((value, index) => index * 50)
-      .y((value) => 150 - value)
+      .x((value, index) => xScale(index))
+      .y(yScale)
       .curve(curveCardinal);
 
     svg
-      .selectAll("path")
+      .selectAll(".line")
       // the square brackets avoid d3 to create a path for each data
       .data([data])
       // .join() create the path element
       .join("path")
-      .attr("d", (value) => myLine(value))
+      .attr("class", "line")
+      .attr("d", myLine)
       .attr("fill", "none")
       .attr("stroke", "blue");
   }, [data]);
 
   return (
     <div className="App">
-      <svg ref={svgRef}></svg>
+      <svg ref={svgRef}>
+        <g className="x-axis" />
+        <g className="y-axis" />
+      </svg>
+      <br />
+      <br />
       <br />
       <button onClick={() => setData(data.map((value) => value + 5))}>
         Upadte data
