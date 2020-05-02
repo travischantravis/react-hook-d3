007c024c1c31cc98253a76c551c65219b0a00dd6
travischantravis <chancheukyin4463@gmail.com>
2020-05-02 13:32:32 -0700

    lesson 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // Let d3 do the DOM instead of react
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { select } from "d3";
 import "./App.css";
 
-const data = [25, 30, 45, 60, 20];
-
 function App() {
   const svgRef = useRef();
+
+  const [data, setData] = useState([25, 30, 45, 60, 20]);
 
   // the empty array suggests that it will only run once
   useEffect(() => {
@@ -24,11 +24,18 @@ function App() {
       .attr("cx", (value) => value * 2)
       .attr("cy", (value) => value * 2)
       .attr("stroke", "red");
-  }, []);
+  }, [data]);
 
   return (
     <div className="App">
       <svg ref={svgRef}></svg>
+      <br />
+      <button onClick={() => setData(data.map((value) => value + 5))}>
+        Upadte data
+      </button>
+      <button onClick={() => setData(data.filter((value) => value <= 5))}>
+        Filter data
+      </button>
     </div>
   );
 }
