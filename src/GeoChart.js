cedcfaf8bef8f06310033acc8847d4a649a142e8
travischantravis <chancheukyin4463@gmail.com>
2020-05-04 13:30:50 -0700

    geo chart [2]

--- src/GeoChart.js
+++ src/GeoChart.js
@@ -32,10 +32,9 @@ function GeoChart({ data, property }) {
       dimensions || wrapperRef.current.getBoundingClientRect();
 
     // projects geo-coordinates on a 2D plane
-    const projection = geoMercator().fitSize(
-      [width, height],
-      selectedCountry || data
-    );
+    const projection = geoMercator()
+      .fitSize([width, height], selectedCountry || data)
+      .precision(100); // improve the quality of zooming out
 
     // takes geojson data,
     // transforms that into the d attribute of a path element
@@ -53,6 +52,22 @@ function GeoChart({ data, property }) {
       .transition()
       .attr("fill", (feature) => colorScale(feature.properties[property]))
       .attr("d", (feature) => pathGenerator(feature));
+
+    // render text
+    svg
+      .selectAll(".label")
+      .data([selectedCountry])
+      .join("text")
+      .attr("class", "label")
+      .text(
+        (feature) =>
+          feature &&
+          feature.properties.name +
+            ": " +
+            feature.properties[property].toLocaleString()
+      )
+      .attr("x", 10)
+      .attr("y", 25);
   }, [data, dimensions, property, selectedCountry]);
   return (
     <div ref={wrapperRef} style={{ marginBottom: "2rem" }}>
